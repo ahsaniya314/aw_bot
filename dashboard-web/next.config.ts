dbf192f791184a8c6c26dcdbbfeac91d2ecfdb94
imanahsaniy <imanahsaniya@gmail.com>
2026-06-27 13:35:30 +0700

    tambah aset prefix

--- dashboard-web/next.config.ts
+++ dashboard-web/next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: "export", // Hasilkan folder 'out' statis untuk disajikan oleh Flask
+  basePath: "/dashboard", // Menentukan base path aplikasi di Flask
+  assetPrefix: "/dashboard", // Menentukan prefix untuk aset statis (CSS/JS)
   images: {
     unoptimized: true,
   },
